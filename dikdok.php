cb16849de9de99bbeb8bdffa6cb2ed141d63a9ce
amixam <robertobrinko@gmail.com>
2023-10-05 09:09:21 +0300

    komentārs class

--- dikdok.php
+++ dikdok.php
@@ -1,1 +1,16 @@
+<?php
+class Komentārs
+{
+    public $id;
+    public $video_id;
+    public $autora_id;
+    public $teksts;
 
+    function __construct($id, $video_id, $autora_id, $teksts)
+    {
+        $this->id = $id;
+        $this->video_id = $video_id;
+        $this->autora_id = $autora_id;
+        $this->teksts = $teksts;
+    }
+}
